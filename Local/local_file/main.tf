cb25be8690418064e17bbc02e9885e45c6af5d92
Hamzi <Hamzi>
2024-02-21 10:23:57 +0500

    Local_file IAC

---
+++ Local/local_file/main.tf
@@ -0,0 +1,10 @@
+resource "local_file" "foo" {
+  content = "How a local file works"
+  filename = "./files/test.txt"
+  file_permission = 0644
+} 
+
+resource "local_file" "name" {
+  content = "path.module: ${path.module}"
+  filename = "./files/file.txt"
+}
